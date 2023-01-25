a8c8eb9471bfa8b8a10cd9c733d0b6024a6706c3
KarimAziev <karim.aziiev@gmail.com>
2023-01-25 14:31:05 +0200

    change layout and description

--- h-transient.el
+++ h-transient.el
@@ -37,37 +37,41 @@
 ;;;###autoload (autoload 'h-transient "h-transient" nil t)
 (transient-define-prefix h-transient ()
   "Command dispatcher for help commands."
-  [["Show help for"
-    ("m" "Major and minor modes"                  describe-mode)
+  ["Describe"
+   [("m" "Mode"                                   describe-mode)
     ("k" "Key"                                    describe-key)
     ("c" "Key briefly"                            describe-key-briefly)
     ("x" "Command"                                describe-command)
     ("f" "Function"                               describe-function)
     ("v" "Variable"                               describe-variable)
-    ("o" "Function or variable"                   describe-symbol)]
-   ["Search for"
-    ("a" "Commands"                               apropos)
-    ("D" "Documentation of functions, variables"  apropos-documentation)
-    ("b" "All key bindings"                       describe-bindings)
-    ("w" "Which key runs a specific command"      where-is)]]
+    ("o" "Symbol"                                 describe-symbol)
+    ("P" "Package"                                describe-package)]
+   [("C-h f" "Faceup"                             faceup-view-buffer)
+    ("C-h k" "Keymap"                             describe-keymap)
+    ("C-h b" "Key bindings"                       describe-bindings)
+    ("C-h P" "Personal bindings"                  describe-personal-keybindings)
+    ("C-h m" "Minor mode"                         describe-minor-mode)
+    ("C-h i" "Minor mode from indicator"          describe-minor-mode-from-indicator)
+    ("C-h s" "Current coding system"              describe-current-coding-system)]]
   [["Show Manual"
     ("r" "Emacs manual"                           info-emacs-manual)
-    ("F" "Emacs manual for command"               Info-goto-emacs-command-node)
-    ("K" "Manual for a key"
-     Info-goto-emacs-key-command-node)
+    ("F" "Manual for command"                     Info-goto-emacs-command-node)
+    ("K" "Manual for a key"                       Info-goto-emacs-key-command-node)
     ("i" "All installed manuals"                  info)
     ("R" "A specific manual"                      info-display-manual)
     ("S" "Symbol manual"                          info-lookup-symbol)
     ("u" "Unix man page"                          manual-entry)]
-   ["Other Help Commands"
-    ("%" "Extending Emacs with external packages" view-external-packages)
-    ("p" "Search for Emacs packages"              finder-by-keyword)
-    ("P" "Describe a specific Emacs package"      describe-package)
-    ("*" "Start the Emacs tutorial"               help-with-tutorial)
+   ["Search for"
+    ("a" "Commands"                               apropos)
+    ("D" "Documentation"                          apropos-documentation)
+    ("w" "Command key"                            where-is)
+    ("p" "Emacs packages"                         finder-by-keyword)]]
+  ["Help"
+    ("t" "Start the Emacs tutorial"                help-with-tutorial)
     ("^" "Display the quick help buffer."         help-quit-or-quick)
     ("e" "Show recent messages"                   view-echo-area-messages)
     ("l" "Show last 300 input keystrokes"         view-lossage)
-    ("." "Show local help at point"               display-local-help)]]
+    ("." "Show local help at point"               display-local-help)]
   [["Miscellaneous"
     ("C-a" "About Emacs"                          about-emacs)
     ("C-f" "Emacs FAQ"                            view-emacs-FAQ)
@@ -79,19 +83,14 @@
     ("C-o" "Distribution information"             describe-distribution)
     ("C-m" "Order printed manuals"                view-order-manuals)
     ("C-t" "Emacs TODO"                           view-emacs-todo)
-    ("C-w" "Information about warranty"           describe-no-warranty)]]
-  [["Describe"
-    ("h" "T[h]eme"                                describe-theme)
-    ("t" "Text properties"                        describe-text-properties)
-    (">" "Char"                                   describe-char)
-    ("O" "Icon"                                   describe-icon)
-    ("E" "Face"                                   describe-face)]
+    ("C-w" "Information about warranty"           describe-no-warranty)]
    ["Internationalization and Coding Systems"
     ("I" "Describe input method"                  describe-input-method)
     ("C" "Describe coding system"                 describe-coding-system)
     ("L" "Describe language environment"          describe-language-environment)
     ("s" "Show current syntax table"              describe-syntax)
-    ("H" "Display the HELLO file"                 view-hello-file)]])
+    ("H" "Display the HELLO file"                 view-hello-file)
+    ("C-h d" "Describe current display table"     describe-current-display-table)]])
 
 (provide 'h-transient)
 ;;; h-transient.el ends here
